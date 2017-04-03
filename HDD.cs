5a79153a992c855c1cb567c08d4a63fe59ab6048
Gor32 <kirakosyan0001@gmail.com>
2017-04-03 14:38:23 +0400

    Update HDD.cs

--- HDD.cs
+++ HDD.cs
@@ -13,11 +13,28 @@ namespace Hardware
     {
         public static readonly int THWB = 15;//thread write bytes
 
-        public static readonly int THRB = 1;//thraed read bytes
+        public static readonly int THRB = 15;//thraed read bytes
 
-        public static event WriteEndHandler WriteHandler = WHand;
+        public static event WriteEndHandler WriteHandler = null;//= WHand;
 
-        public static event ReadEndHandler ReadHandler = RHand;
+        public static event ReadEndHandler ReadHandler = null;// = RHand;
+
+        public static bool isNullWriteHandler() {
+
+            if(WriteHandler == null)
+                return true;
+            
+            return false;
+        }
+
+        public static bool isNullReadHandler()
+        {
+
+            if (ReadHandler == null)
+                return true;
+
+            return false;
+        }
 
         public static void ThreadCallWrite(Object threadContext)
         {
@@ -26,8 +43,8 @@ namespace Hardware
             for (int i = 0; i < threadValue.length; i++)
                 lock (fs)
                 {
-                    fs.Seek(i * OFFSET + threadValue.start + i, SeekOrigin.Begin);
-                    fs.WriteByte(threadValue.data[i]);
+                    fs.Seek( threadValue.start + i, SeekOrigin.Begin);///////////nayell OFFSET urish  arjeqneri depqum
+                    fs.WriteByte(threadValue.data[i]);                    
                 };
 
             Console.WriteLine("write thread run");
@@ -40,7 +57,11 @@ namespace Hardware
                 {
                     cCTEndWTs = 0;
                     fs.Close();
-                    WriteHandler();
+                    if (WriteHandler != null)
+                    {
+                        WriteHandler();
+                        WriteHandler = null;
+                    }
                 }
             }
 
@@ -56,6 +77,8 @@ namespace Hardware
                 else break;
             }
 
+            WriteHandler = WHand;//NAYEL
+
             fs = File.Open(HDD.path, FileMode.Open, FileAccess.Write);
 
             if (length == -1)
@@ -65,14 +88,17 @@ namespace Hardware
 
             if (length < THWB)
                 countWrite = 1;
-            else countWrite = length / THWB;
+            else countWrite = length / THWB+1;
 
 
             while (length > 0)
             {
                 Thread newThraed = new Thread(HDD.ThreadCallWrite);
 
-                byte[] dataThread = new byte[length];
+              
+
+                byte[] dataThread = new byte[length < THWB ? length : THWB];
+
 
                 if (length <= THWB)
                 {
@@ -82,9 +108,11 @@ namespace Hardware
                         dataThread[i] = data[i + currentPosition];
                     }
 
-                    newThraed.Start(new ToWrite(dataThread, (currentPosition + start), length - 1));
+                    newThraed.Start(new ToWrite(dataThread, (currentPosition + start), length - 1));//NAYEL
 
                     currentPosition += length;
+
+                    length -= THWB;
 
                     break;
                 }
@@ -103,18 +131,17 @@ namespace Hardware
 
         }
 
-        public static void ThreadCallRead(Object threadContext)
+
+        public static void ThreadCallRead( Object threadContext)
         {
             ToRead threadValue = (ToRead)threadContext;
 
-            for (int i = 0; i < threadValue.count; i++)
-                lock (fs)
-                {
-                    fs.Seek(i * OFFSET + threadValue.offset + i, SeekOrigin.Begin);
-
-                    threadValue.data[i + threadValue.index] = (byte)fs.ReadByte();
-
-                };
+            lock (fs)
+            {
+                fs.Seek( threadValue.offset , SeekOrigin.Begin);
+                
+                fs.Read(threadValue.data,threadValue.index, threadValue.count);
+             };
 
             Console.WriteLine("raed thread run");
 
@@ -126,7 +153,11 @@ namespace Hardware
                 {
                     cCTEndRTs = 0;
                     fs.Close();
-                    ReadHandler();
+                    if (ReadHandler != null)
+                    {
+                        ReadHandler();
+                        ReadHandler = null;
+                    }
                 }
             }
 
@@ -143,36 +174,41 @@ namespace Hardware
                 else break;
             }
 
+            ReadHandler = RHand;//NAYEL
+
             fs = File.Open(path, FileMode.Open, FileAccess.Read);
 
             int currentPosition = 0;
 
-            if (count < THWB)
-                countRead = 1;
-            else countRead = count / THWB;
+            countRead = count < THRB ? 1 : count / THRB+1;
 
-
+            Console.WriteLine("hesasdfhasjk   "+countRead);
             while (count > 0)
             {
                 Thread newThraed = new Thread(HDD.ThreadCallRead);
 
-                if (count <= THWB)
+                if (count <= THRB)
                 {
-                    newThraed.Start(new ToRead(ref data, (currentPosition + offset), count - 1, currentPosition));
+                    newThraed.Start((Object)new ToRead(ref data, (currentPosition + offset), count - 1, currentPosition));
 
                     currentPosition += count;
+
+                    count -= THWB;
 
                     break;
                 }
 
-                newThraed.Start(new ToRead(ref data, currentPosition + count, THRB, currentPosition));
+                newThraed.Start((Object)new ToRead(ref data, currentPosition + offset, THRB, currentPosition));
 
-                currentPosition += THWB;
+                currentPosition += THRB;
 
-                count -= THWB;
+                count -= THRB;
             }
 
         }
+
+
+
 
 
         //file must be creat
@@ -221,6 +257,7 @@ namespace Hardware
                 this.offset = offset;
                 this.count = count;
                 this.data = data;
+              
             }
             public byte[] data;
             public int index;
@@ -237,17 +274,20 @@ namespace Hardware
     {
         public static void Main(string[] args)
         {
-            int intValue = 13;
+            int intValue = 82;
             byte[] intValueOnByteArray = BitConverter.GetBytes(intValue);
 
-            HDD.Write(intValueOnByteArray, 18);
+           // HDD.Write(intValueOnByteArray, 18);
 
-            intValue = 14;
-            intValueOnByteArray = BitConverter.GetBytes(intValue);
+            intValue = 19;
+            //intValueOnByteArray = BitConverter.GetBytes(intValue);
             HDD.Write(intValueOnByteArray, 24);
 
+
+            byte[] h = new byte[40];
+
             byte[] stringOnBytes;
-   
+
             string s = "hes1a ";
 
             for (int i = 0; i < s.Length; i++)
@@ -256,12 +296,17 @@ namespace Hardware
                 HDD.Write(stringOnBytes, i);
             }
 
-            HDD.Read(ref intValueOnByteArray,18,4);
+            HDD.Read(ref h, 0,40);
+           
+            //for (int i = 0; i < 100000000; i++) ;
 
-            foreach(var i in intValueOnByteArray)
-                Console.WriteLine(i);
+            while (!HDD.isNullReadHandler()) ;
+            for (int i = 0; i < h.Length; i++)
+            {
+                Console.Write(i); Console.WriteLine( "  "+h[i]);
+            }
 
-
+            
         }
     }
 }
