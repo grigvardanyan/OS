433f87debac35a3bad7db2567d2fa559a92c2614
Gor32 <kirakosyan0001@gmail.com>
2017-04-16 15:46:47 +0400

    Update SuperBlock.cs

--- FileSystem/SuperBlock.cs
+++ FileSystem/SuperBlock.cs
@@ -1,12 +1,35 @@
-using System;
 using Hardware;
-namespace FileSystem
+using System;
+
+partial class FileSystem
 {
-    //starting on 0 position
-    class SuperBlock
+    public class SuperBlock
     {
-        //blockSize
-        private static int blockSize = -1;//4096
+        public static void Init()
+        {
+			//for test
+            blockSize = 100;
+            maxBlocks = 32;
+            blockStart = 0;
+            inodeStart = 100 * 32;
+            maxInodesCount = 23;
+            inodeSize = 45;
+            inodeDirStart = maxInodesCount * inodeSize + inodeStart;
+            maxInodesDirCount = 12;
+            inodeDirSize = 32;
+            FSMstart = maxInodesCount * inodeDirSize + inodeDirStart;
+            FSMsize = 256;
+            inodeTableStart = FSMsize + FSMstart;
+            inodeTableSize = 16;
+            blockMaxCountInInode = 12;
+            inodeMaxCountInDir = 2;
+            dirMaxCountInDir = 1;
+
+        }
+
+
+
+        private static int blockSize = -1;  //1024
         public static int BlockSize
         {
             get
@@ -14,35 +37,15 @@ namespace FileSystem
                 if (blockSize == -1)
                 {
                     byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 0);
-                    while (!HDD.isNullReadHandler()) ;
 
+                    HardDisk.Read(ref buffer, 0);
                     blockSize = BitConverter.ToInt32(buffer, 0);
                 }
                 return blockSize;
             }
         }
 
-        //blockCountInInodes
-        private static int blockCountInInodes = -1;//1024
-        public static int BlockCountInInodes
-        {
-            get
-            {
-                if (blockCountInInodes == -1)
-                {
-                    byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 4);
-                    while (!HDD.isNullReadHandler()) ;
-
-                    blockCountInInodes = BitConverter.ToInt32(buffer, 0);
-                }
-                return blockCountInInodes;
-            }
-        }
-
-        //maxBlocks
-        private static int maxBlocks = -1;//1024*128
+        private static int maxBlocks = -1; //32
         public static int MaxBlocks
         {
             get
@@ -50,16 +53,14 @@ namespace FileSystem
                 if (maxBlocks == -1)
                 {
                     byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 8);
-                    while (!HDD.isNullReadHandler()) ;
 
+                    HardDisk.Read(ref buffer, 4);
                     maxBlocks = BitConverter.ToInt32(buffer, 0);
                 }
                 return maxBlocks;
             }
         }
 
-        //blockStart
         private static int blockStart = -1;//0
         public static int BlockStart
         {
@@ -68,49 +69,14 @@ namespace FileSystem
                 if (blockStart == -1)
                 {
                     byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 12);
-                    while (!HDD.isNullReadHandler()) ;
-
+                    HardDisk.Read(ref buffer, 8);
                     blockStart = BitConverter.ToInt32(buffer, 0);
                 }
                 return blockStart;
             }
         }
 
-        //freeBlocksCount
-        private static int freeBlocksCount = -1;//1024*128-1
-        private static bool boolFreeBlockCount = false;
-        public static int FreeBlocksCount
-        {
-            get
-            {
-                if (!boolFreeBlockCount)
-                {
-
-                    boolFreeBlockCount = true;
-                    byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 16);
-                    while (!HDD.isNullReadHandler()) ;
-
-                    freeBlocksCount = BitConverter.ToInt32(buffer, 0);
-                }
-                return freeBlocksCount;
-            }
-
-            set
-            {
-                boolFreeBlockCount = false;
-                freeBlocksCount = value;
-                byte[] buffer = new byte[4];
-                buffer = BitConverter.GetBytes(freeBlocksCount);
-                HDD.Write(buffer, 16);
-                while (!HDD.isNullWriteHandler()) ;
-
-            }
-        }
-
-        //inodeStart
-        private static int inodeStart = -1;//4096*128*1024 
+        private static int inodeStart = -1;// 32*1024+0
         public static int InodeStart
         {
             get
@@ -118,17 +84,16 @@ namespace FileSystem
                 if (inodeStart == -1)
                 {
                     byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 20);
-                    while (!HDD.isNullReadHandler()) ;
 
+                    HardDisk.Read(ref buffer, 12);
                     inodeStart = BitConverter.ToInt32(buffer, 0);
                 }
+
                 return inodeStart;
             }
         }
 
-        //maxInodesCount
-        private static int maxInodesCount = -1;//128
+        private static int maxInodesCount = -1;//16
         public static int MaxInodesCount
         {
             get
@@ -136,8 +101,7 @@ namespace FileSystem
                 if (maxInodesCount == -1)
                 {
                     byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 24);
-                    while (!HDD.isNullReadHandler()) ;
+                    HardDisk.Read(ref buffer, 16);
 
                     maxInodesCount = BitConverter.ToInt32(buffer, 0);
                 }
@@ -145,36 +109,7 @@ namespace FileSystem
             }
         }
 
-        //currentInodesCount
-        private static int currentInodesCount = -1;
-        private static bool boolCurrentInodesCount = false;
-        public static int CurrentInodesCount
-        {
-            get
-            {
-                if (!boolCurrentInodesCount)
-                {
-                    boolCurrentInodesCount = true;
-                    byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 28);
-                    while (!HDD.isNullReadHandler()) ;
-
-                    currentInodesCount = BitConverter.ToInt32(buffer, 0);
-                }
-                return currentInodesCount;
-            }
-            set
-            {
-                boolCurrentInodesCount = false;
-                byte[] buffer = new byte[4];
-                buffer = BitConverter.GetBytes(value);
-                HDD.Write(buffer, 28);
-                while (!HDD.isNullReadHandler()) ;
-            }
-        }
-
-        //inodeSize
-        private static int inodeSize = -1; //4102
+        private static int inodeSize = -1;/// //////////////////////////////////////////////////
         public static int InodeSize
         {
             get
@@ -182,105 +117,14 @@ namespace FileSystem
                 if (inodeSize == -1)
                 {
                     byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 32);
-                    while (!HDD.isNullReadHandler()) ;
-
+                    HardDisk.Read(ref buffer, 20);
                     inodeSize = BitConverter.ToInt32(buffer, 0);
                 }
                 return inodeSize;
             }
         }
 
-        //freeSpaceMgmtStart
-        private static int freeSpaceMgmtStart = -1;//4102*128 + (4096*1024*128 )
-        public static int FreeSpaceMgmtStart
-        {
-            get
-            {
-                if (freeSpaceMgmtStart == -1)
-                {
-                    byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 36);
-                    while (!HDD.isNullReadHandler()) ;
-
-                    freeSpaceMgmtStart = BitConverter.ToInt32(buffer, 0);
-                }
-                return freeSpaceMgmtStart;
-            }
-        }
-
-        //freeSpaceMgmtSize
-        private static int freeSpaceMgmtSize = -1;//1024*128
-        public static int FreeSpaceMgmtSize
-        {
-            get
-            {
-                if (freeSpaceMgmtSize == -1)
-                {
-                    byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 40);
-                    while (!HDD.isNullReadHandler()) ;
-
-                    freeSpaceMgmtSize = BitConverter.ToInt32(buffer, 0);
-                }
-                return freeSpaceMgmtSize;
-            }
-        }
-
-        //root
-        private static int root = -1; // 1024*128  +(4102*128 + (4096*1024*128 ))
-        public static int Root
-        {
-            get
-            {
-                if (root == -1)
-                {
-                    byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 44);
-                    while (!HDD.isNullReadHandler()) ;
-
-                    root = BitConverter.ToInt32(buffer, 0);
-                }
-                return root;
-            }
-        }
-
-        private static int inodeTableStart = -1;// 4 + 1024*128  +(4102*128 + (4096*1024*128 )) 
-        public static int InodeTableStart
-        {
-            get
-            {
-                if (inodeTableStart == -1)
-                {
-                    byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 48);
-                    while (!HDD.isNullReadHandler()) ;
-
-                    inodeTableStart = BitConverter.ToInt32(buffer, 0);
-                }
-                return inodeTableStart;
-            }
-        }
-
-
-        private static int inodeTableSize = -1;//128 
-        public static int InodeTableSize
-        {
-            get
-            {
-                if (inodeTableSize == -1)
-                {
-                    byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 52);
-                    while (!HDD.isNullReadHandler()) ;
-
-                    inodeTableSize = BitConverter.ToInt32(buffer, 0);
-                }
-                return inodeTableSize;
-            }
-        }
-
-        private static int inodeDirStart = -1;// 128 + 4 + 1024*128  +(4102*128 + (4096*1024*128 ))
+        private static int inodeDirStart = -1;// 
         public static int InodeDirStart
         {
             get
@@ -288,17 +132,32 @@ namespace FileSystem
                 if (inodeDirStart == -1)
                 {
                     byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 56);
-                    while (!HDD.isNullReadHandler()) ;
 
+                    HardDisk.Read(ref buffer, 24);
                     inodeDirStart = BitConverter.ToInt32(buffer, 0);
                 }
+
                 return inodeDirStart;
             }
-           
         }
 
-        private static int inodeDirSize = -1;// 32*80 
+        private static int maxInodesDirCount = -1;//128
+        public static int MaxInodesDirCount
+        {
+            get
+            {
+                if (maxInodesDirCount == -1)
+                {
+                    byte[] buffer = new byte[4];
+                    HardDisk.Read(ref buffer, 28);
+
+                    maxInodesDirCount = BitConverter.ToInt32(buffer, 0);
+                }
+                return maxInodesDirCount;
+            }
+        }
+
+        private static int inodeDirSize = -1;//
         public static int InodeDirSize
         {
             get
@@ -306,16 +165,93 @@ namespace FileSystem
                 if (inodeDirSize == -1)
                 {
                     byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 60);
-                    while (!HDD.isNullReadHandler()) ;
-
+                    HardDisk.Read(ref buffer, 32);
                     inodeDirSize = BitConverter.ToInt32(buffer, 0);
                 }
                 return inodeDirSize;
             }
         }
 
-        private static int inodeDirTableStart = -1;// 32*80 + 128 + 4 + 1024*128  +(4102*128 + (4096*1024*128 ))
+        private static int FSMstart = -1;
+        public static int FSMStart
+        {
+            get
+            {
+                if (FSMstart == -1)
+                {
+                    byte[] buffer = new byte[4];
+                    HardDisk.Read(ref buffer, 36);
+                    FSMstart = BitConverter.ToInt32(buffer, 0);
+                }
+                return FSMstart;
+            }
+        }
+
+        private static int FSMsize = -1;
+        public static int FSMSize
+        {
+            get
+            {
+                if (FSMsize == -1)
+                {
+                    byte[] buffer = new byte[4];
+                    HardDisk.Read(ref buffer, 40);
+                    FSMsize = BitConverter.ToInt32(buffer, 0);
+                }
+                return FSMsize;
+            }
+        }
+
+        private static int root = -1;
+        public static int Root
+        {
+            get
+            {
+                if (root == -1)
+                {
+                    byte[] buffer = new byte[4];
+                    HardDisk.Read(ref buffer, 44);
+
+                    root = BitConverter.ToInt32(buffer, 0);
+                }
+                return root;
+            }
+        }
+
+        private static int inodeTableStart = -1;
+        public static int InodeTableStart
+        {
+            get
+            {
+                if (inodeTableStart == -1)
+                {
+                    byte[] buffer = new byte[4];
+                    HardDisk.Read(ref buffer, 48);
+
+                    inodeTableStart = BitConverter.ToInt32(buffer, 0);
+                }
+
+                return inodeTableStart;
+            }
+        }
+
+        private static int inodeTableSize = -1;
+        public static int InodeTableSize
+        {
+            get
+            {
+                if (inodeTableSize == -1)
+                {
+                    byte[] buffer = new byte[4];
+                    HardDisk.Read(ref buffer, 52);
+
+                    inodeTableSize = BitConverter.ToInt32(buffer, 0);
+                }
+                return inodeTableSize;
+            }
+        }
+
+        private static int inodeDirTableStart = -1;
         public static int InodeDirTableStart
         {
             get
@@ -323,8 +259,7 @@ namespace FileSystem
                 if (inodeDirTableStart == -1)
                 {
                     byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 64);
-                    while (!HDD.isNullReadHandler()) ;
+                    HardDisk.Read(ref buffer, 56);
 
                     inodeDirTableStart = BitConverter.ToInt32(buffer, 0);
                 }
@@ -341,8 +276,7 @@ namespace FileSystem
                 if (inodeDirTableSize == -1)
                 {
                     byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, 68);
-                    while (!HDD.isNullReadHandler()) ;
+                    HardDisk.Read(ref buffer, 60);
 
                     inodeDirTableSize = BitConverter.ToInt32(buffer, 0);
                 }
@@ -350,7 +284,156 @@ namespace FileSystem
             }
         }
 
+        private static int blockMaxCountInInode = -1; //SuperBlock.MaxInodesCount / SuperBlock.MaxBlocks
+        public static int BlockMaxCountInInode
+        {
+            get
+            {
+                if (blockMaxCountInInode == -1)
+                {
+                    byte[] buffer = new byte[4];
+                    HardDisk.Read(ref buffer, 64);
+
+                    blockMaxCountInInode = BitConverter.ToInt32(buffer, 0);
+                }
+                return blockMaxCountInInode;
+            }
+        }
+
+        private static int inodeMaxCountInDir = -1;//SuperBlock.MaxInodesCount / SuperBlock.MaxInodesDirCount
+        public static int InodeMaxCountInDir
+        {
+            get
+            {
+                if (inodeMaxCountInDir == -1)
+                {
+                    byte[] buffer = new byte[4];
+                    HardDisk.Read(ref buffer, 68);
+
+                    inodeMaxCountInDir = BitConverter.ToInt32(buffer, 0);
+                }
+                return inodeMaxCountInDir;
+            }
+        }
+
+        private static int dirMaxCountInDir = -1;
+        public static int DirMaxCountInDir
+        {
+            get
+            {
+                if (dirMaxCountInDir == -1)
+                {
+                    byte[] buffer = new byte[4];
+                    HardDisk.Read(ref buffer, 72);
+
+                    dirMaxCountInDir = BitConverter.ToInt32(buffer, 0);
+                }
+                return dirMaxCountInDir;
+            }
+        }
+
+
+        //--------------------------------------------------------add
+        private static int inodeNameAndIDTableStart = -1;
+        public static int InodeNameAndIDTableStart
+        {
+            get
+            {
+                if (inodeNameAndIDTableStart == -1)
+                {
+                    byte[] buffer = new byte[4];
+                    HardDisk.Read(ref buffer, 76);
+
+                    inodeNameAndIDTableStart = BitConverter.ToInt32(buffer, 0);
+                }
+                return inodeNameAndIDTableStart;
+            }
+        }
+
+        private static int inodeDirNameAndIDTableStart = -1;
+        public static int InodeDirNameAndIDTableStart
+        {
+            get
+            {
+                if (inodeDirNameAndIDTableStart == -1)
+                {
+                    byte[] buffer = new byte[4];
+                    HardDisk.Read(ref buffer, 80);
+
+                    inodeDirNameAndIDTableStart = BitConverter.ToInt32(buffer, 0);
+                }
+                return inodeDirNameAndIDTableStart;
+            }
+        }
+
+        private static int inodeNameLength = -1;
+        public static int InodeNameLength
+        {
+            get
+            {
+                if (inodeNameLength == -1)
+                {
+                    byte[] buffer = new byte[4];
+                    HardDisk.Read(ref buffer, 84);
+
+                    inodeNameLength = BitConverter.ToInt32(buffer, 0);
+                }
+                return inodeNameLength;
+            }
+        }
+
+        private static int inodeDirNameLength = -1;
+        public static int InodeDirNameLength
+        {
+            get
+            {
+                if (inodeDirNameLength == -1)
+                {
+                    byte[] buffer = new byte[4];
+                    HardDisk.Read(ref buffer, 88);
+
+                    inodeDirNameLength = BitConverter.ToInt32(buffer, 0);
+                }
+                return inodeDirNameLength;
+            }
+        }
+
+
+        private static int inodeNameAndIDTableSize = -1;
+        public static int InodeNameAndIDTableSize
+        {
+            get
+            {
+                if (inodeNameAndIDTableSize == -1)
+                {
+                    byte[] buffer = new byte[4];
+                    HardDisk.Read(ref buffer, 92);
+
+                    inodeNameAndIDTableSize = BitConverter.ToInt32(buffer, 0);
+                }
+                return inodeNameAndIDTableSize;
+            }
+        }
+
+        private static int inodeDirNameAndIDTableSize = -1;
+        public static int InodeDirNameAndIDTableSize
+        {
+            get
+            {
+                if (inodeDirNameAndIDTableSize == -1)
+                {
+                    byte[] buffer = new byte[4];
+                    HardDisk.Read(ref buffer, 96);
+
+                    inodeDirNameAndIDTableSize = BitConverter.ToInt32(buffer, 0);
+                }
+                return inodeDirNameAndIDTableSize;
+            }
+        }
+
+
     }
+
 
 
 }
