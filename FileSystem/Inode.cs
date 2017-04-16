91173f4156e7162fb08f42118c3e067e4718cbe5
Gor32 <kirakosyan0001@gmail.com>
2017-04-16 15:44:32 +0400

    Update Inode.cs

--- FileSystem/Inode.cs
+++ FileSystem/Inode.cs
@@ -1,136 +1,43 @@
-using System;
 using Hardware;
+using System;
 
-namespace FileSystem
+partial class FileSystem
 {
-    
-    class Inode
+    public class Inode
     {
-        //ID > 128 error 
+        private int[] arrayBlocks = new int[SuperBlock.BlockMaxCountInInode];
+
+        private byte fileID;
+
         public Inode(byte ID = 255)
         {
             if (ID == 255)
             {
-                fileID = InodeTable.GetID();
-                 Console.WriteLine(fileID);
+                fileID = (byte)InodeTable.GetInode();
+          
             }
             else fileID = ID;
         }
 
-        private int[] arrayBlocks = new int[1024];//block numbers
         public int GetArrayBlock(int index)
         {
 
             byte[] buffer = new byte[4];
-            HDD.Read(ref buffer, SuperBlock.InodeStart + fileID * SuperBlock.InodeSize + 4 * index);
+            HardDisk.Read(ref buffer, SuperBlock.InodeStart + fileID * SuperBlock.InodeSize + 4 * index);
             arrayBlocks[index] = BitConverter.ToInt32(buffer, 0);
 
             return arrayBlocks[index];
         }
 
-        // index <= BlockCount
-        public void SetArrayBlock(int index, int number = -1) // index , number block
+       
+        public void SetArrayBlock(int index, int number) 
         {
             byte[] buffer = new byte[4];
-            
+            buffer = BitConverter.GetBytes(number*SuperBlock.BlockSize+SuperBlock.BlockStart);
 
-			buffer = BitConverter.GetBytes(number);
-            HDD.Write(buffer, SuperBlock.InodeStart + fileID * SuperBlock.InodeSize + index);
+            HardDisk.Write(buffer, SuperBlock.InodeStart + fileID * SuperBlock.InodeSize + 4 * index);
 
-			if (number == -1) {
-				FreeSpaceMgmt.SetArrayBlock (arrayBlocks [index], 0);
-				blockCount = blockCount - 1;
-				arrayBlocks[index] = 0;
-
-			} else {
-				blockCount = blockCount + 1;
-				FreeSpaceMgmt.SetArrayBlock (number, 1);//update
-				arrayBlocks[index] = number;
-			}            
-        }
-
-   		private byte fileID;
-
-        private int blockCount;//= -1;
-        private bool boolBlockCount = false;
-        public int BlockCount
-        {
-            get
-            {
-                if (!boolBlockCount)
-                {
-                    boolBlockCount = true;
-                    byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, SuperBlock.InodeStart + fileID * SuperBlock.InodeSize + 4096);
-                    while (!HDD.isNullReadHandler()) ;
-
-                    blockCount = BitConverter.ToInt32(buffer, 0);
-                }
-                return blockCount;
-            }
-            set
-            {
-                boolBlockCount = false;
-                byte[] buffer = new byte[4];
-                buffer = BitConverter.GetBytes(value);
-                HDD.Write(buffer, SuperBlock.InodeStart + fileID * SuperBlock.InodeSize + 4096);
-                while (!HDD.isNullWriteHandler()) ;
-
-                blockCount = value;
-            }
-        }
-
-        private int modifyTime;
-        private bool boolModifyTime = false;
-        public int ModifyTime
-        {
-            get
-            {
-                if (!boolModifyTime)
-                {
-                    boolModifyTime = true;
-                    byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, SuperBlock.InodeStart + fileID * SuperBlock.InodeSize + 4096 + 4);
-                    while (!HDD.isNullReadHandler()) ;
-                    modifyTime = BitConverter.ToInt32(buffer, 0);
-                }
-                return modifyTime;
-            }
-            set
-            {
-                boolModifyTime = false;
-                byte[] buffer = new byte[4];
-                HDD.Write(buffer, SuperBlock.InodeStart + fileID * SuperBlock.InodeSize + 4096 + 4);
-                while (!HDD.isNullWriteHandler()) ;
-                modifyTime = BitConverter.ToInt32(buffer, 0);
-            }
-        }
-
-        private int accessTime;
-        private bool boolAccessTime = false;
-        public int AccessTime
-        {
-
-            get
-            {
-                if (!boolAccessTime)
-                {
-                    boolAccessTime = true;
-                    byte[] buffer = new byte[4];
-                    HDD.Read(ref buffer, SuperBlock.InodeStart + fileID * SuperBlock.InodeSize + 4096 + 4 + 4);
-                    while (!HDD.isNullReadHandler()) ;
-                    accessTime = BitConverter.ToInt32(buffer, 0);
-                }
-				return accessTime;
-            }
-            set
-            {
-                boolAccessTime = false;
-                byte[] buffer = new byte[4];
-                HDD.Write(buffer, SuperBlock.InodeStart + fileID * SuperBlock.InodeSize + 4096 + 4 + 4);
-                while (!HDD.isNullWriteHandler()) ;
-                accessTime = BitConverter.ToInt32(buffer, 0);
-            }
+            arrayBlocks[index] = number * SuperBlock.BlockSize + SuperBlock.BlockStart;
         }
     }
 }
