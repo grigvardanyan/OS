c1870503406726fbc7d1303653bf2f6212376eb3
Gor32 <kirakosyan0001@gmail.com>
2017-04-16 15:45:25 +0400

    Update InodeDirTable.cs

--- FileSystem/InodeDirTable.cs
+++ FileSystem/InodeDirTable.cs
@@ -1,39 +1,33 @@
-using System;
 using Hardware;
 
-namespace FileSystem
+partial class FileSystem
 {
-	class InodeDirTable
-	{
-		private static byte[] arrayDirID = new byte[32]; // 32 hat direvtoria
-		public static byte GetArrayDirID(int index)
-		{
-			byte[] buffer = new byte[1];
-			HDD.Read(ref buffer,SuperBlock.InodeDirTableStart+index);
-			while (!HDD.isNullReadHandler()) ;
+    class InodeDirTable
+    {
+        private static byte[] arrayInodeDirTable = new byte[SuperBlock.MaxInodesDirCount];
+        public static void SetArrayInodeDirTable(int index, byte value)
+        {
+            arrayInodeDirTable[index] = value;
+            HardDisk.Write(new byte[1] { value }, SuperBlock.InodeDirTableStart + index);
+        }
+        public static byte GetArrayInodeDirTable(int index)
+        {
+            byte[] buffer = new byte[1];
+            HardDisk.Read(ref buffer, SuperBlock.InodeDirTableStart + index);
+            arrayInodeDirTable[index] = buffer[0];
+            return arrayInodeDirTable[index];
+        }
 
-			arrayDirID[index] = buffer[0];
-			return arrayDirID[index];
-		}
-		public static void SetArrayDirID(int index, byte value)
-		{
-			arrayDirID[index] = value;
-			HDD.Write(new byte[1] { value }, SuperBlock.InodeDirTableStart+index);
-			while (!HDD.isNullWriteHandler()) ;
-		}
-		public static byte GetDirID()
-		{
+        public static int GetInodeDir()
+        {
 
-			for (byte i = 0; i < arrayDirID.Length; i++)
-				if (GetArrayDirID(i) == 0)
-				{
-					SetArrayDirID(i, 1);
-					return i;
-				}
-
-			return 0;
-		}
-
-	}
+            for (int i = 0; i < arrayInodeDirTable.Length; i++)
+                if (GetArrayInodeDirTable(i) == 0)
+                {
+                    SetArrayInodeDirTable(i, 1);
+                    return i;
+                }
+            return 0;
+        }
+    }
 }
-
